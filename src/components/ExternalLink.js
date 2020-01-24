6e7e5822214ab312972fc06753f0a7e6dbf29692
Chris Whong <chris.m.whong@gmail.com>
2020-01-24 16:12:50 -0500

    feat: add ga custom event for download

--- src/components/ExternalLink.js
+++ src/components/ExternalLink.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
-const ExternalLink = ({ className, to, children }) => (
-  <a className={className} href={to} target='_blank' rel='noopener noreferrer'>{children}</a>
+const ExternalLink = ({ className, to, children, onClick = () => {} }) => (
+  <a
+    className={className}
+    href={to}
+    target='_blank'
+    rel='noopener noreferrer'
+    onClick={onClick}
+  >
+    {children}
+  </a>
 )
 
 export default ExternalLink
