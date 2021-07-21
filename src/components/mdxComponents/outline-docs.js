5895e66135c45a731fc9804eaebf1f1068006bc3
Chris Whong <chris.m.whong@gmail.com>
2021-07-21 17:36:56 -0400

    feat: add sample http docs

--- src/components/mdxComponents/outline-docs.js
+++ src/components/mdxComponents/outline-docs.js
@@ -4,10 +4,10 @@ import CodeBlock from './codeBlock'
 
 export default {
   // h1: props => <h1 className='heading1' id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />,
-  h2: props => <h2 className='text-qrigray-900 font-semibold tracking-wide mt-12 mb-6 text-2xl' {...props} />,
-  h3: props => <h3 className='text-qrigray-800 font-semibold mt-12 mb-6 text-xl' {...props} />,
-  h4: props => <h4 className='text-qrigray-700 font-medium mt-12 mb-6 text-lg' {...props} />,
-  h5: props => <h5 className='text-qrigray-600 font-medium mb-1' {...props} />,
+  h1: props => <h1 className='text-qrigray-900 font-semibold tracking-wide mt-12 mb-6 text-2xl' {...props} />,
+  h2: props => <h2 className='text-qrigray-800 font-semibold mt-12 mb-6 text-xl' {...props} />,
+  h3: props => <h3 className='text-qrigray-700 font-medium mt-12 mb-6 text-lg' {...props} />,
+  h4: props => <h4 className='text-qrigray-600 font-medium mb-1' {...props} />,
   // h6: props => <h6 className='heading6' id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />,
   code: CodeBlock,
   li: props => <li className='mb-3' {...props} />,
